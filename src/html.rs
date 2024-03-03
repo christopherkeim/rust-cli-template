eb39cfbf1e800d6835df6f85ef299c41247551c9
Christopher Keim <122534665+christopherkeim@users.noreply.github.com>
2024-03-03 15:06:12 -0500

    Added `extract_html_from_webpage()`.

---
+++ src/html.rs
@@ -0,0 +1,16 @@
+use reqwest;
+
+/// Makes a GET request and returns its body as a String.
+///
+/// # Arguments
+/// `url: String` URL of the target webpage
+pub fn extract_html_from_webpage(url: String) -> Result<String, reqwest::Error> {
+    let response: reqwest::blocking::Response = match reqwest::blocking::get(url) {
+        Ok(response) => response,
+        Err(http_error) => return Err(http_error),
+    };
+    match response.text() {
+        Ok(html) => return Ok(html),
+        Err(body_extract_failure) => return Err(body_extract_failure),
+    };
+}
