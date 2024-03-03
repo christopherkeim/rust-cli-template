717fe8e3cf3c89eb865fab3a67822831b01edc7b
Christopher Keim <122534665+christopherkeim@users.noreply.github.com>
2024-03-03 17:46:35 -0500

    Changed `file_name` to `&str`.

--- src/files.rs
+++ src/files.rs
@@ -6,7 +6,7 @@ use std::io::Write;
 /// # Arguments
 /// `markdown: String` a string holding markdown
 /// `file_name: String` name of output file
-pub fn write_markdown_file(markdown: String, file_name: String) -> Result<(), std::io::Error> {
+pub fn write_markdown_file(markdown: String, file_name: &str) -> Result<(), std::io::Error> {
     let mut md_file: File = match File::create(file_name) {
         Ok(md_file) => md_file,
         Err(file_creation_error) => return Err(file_creation_error),
