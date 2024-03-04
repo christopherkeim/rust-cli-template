1f3a07acc7eafd2d5804f510ff62fad930ff609d
Christopher Keim <122534665+christopherkeim@users.noreply.github.com>
2024-03-03 23:14:25 -0500

    Added `HtmlConversionError` and error handling.

--- src/markdown.rs
+++ src/markdown.rs
@@ -1,4 +1,21 @@
 use html2md;
+use std::fmt;
+
+/// Errors that occur during html conversion markdown
+pub enum HtmlConversionError {
+    /// Empty string passed as HTML
+    EmptyHtml,
+}
+
+impl fmt::Display for HtmlConversionError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            HtmlConversionError::EmptyHtml => {
+                write!(f, "Emtpy string passed as `html`.")
+            }
+        }
+    }
+}
 
 /// Converts an HTML formatted string to markdown.
 ///
@@ -7,6 +24,10 @@ use html2md;
 ///
 /// # Reference
 /// https://crates.io/crates/html2md
-pub fn convert_html_to_markdown(html: String) -> String {
-    return html2md::parse_html(&html);
+pub fn convert_html_to_markdown(html: String) -> Result<String, HtmlConversionError> {
+    if html.is_empty() {
+        return Err(HtmlConversionError::EmptyHtml);
+    }
+
+    return Ok(html2md::parse_html(&html));
 }
